b2123b4753240d046b180e1648d7a2bfcecdf300
Anwar Numa <anwarnuma@hotmail.com>
2019-08-22 18:12:36 -0400

    Initial Commit

---
+++ Mental_Chess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mental_Chess
+//
+//  Created by Anwar Numa on 8/22/19.
+//  Copyright © 2019 Anwar Numa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
